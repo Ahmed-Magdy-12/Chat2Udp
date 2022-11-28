6280d6655bf630d48fd3789790490457bdfba0d4
Ahmed-Magdy-12 <mhmedshalaby1234@gmail.com>
2022-11-28 14:21:24 +0200

    Chat 2

---
+++ widget.cpp
@@ -0,0 +1,72 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+// dfines
+#define local QHostAddress::LocalHost
+#define portlocal 1244
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Chat 2");
+
+    mSocket = new QUdpSocket(this);
+    mSocket->bind(local,portlocal);
+    connect(mSocket, SIGNAL(readyRead()), this, SLOT(leeSocket()));
+
+    // PlainTextEdit
+    ui->plainTextEdit->setReadOnly(true);
+    ui->plainTextEdit->setStyleSheet("background-color:rgb(255,201,202");
+    ui->plainTextEdit->appendPlainText("My port is:" + QHostAddress(local).toString());
+    ui->plainTextEdit->appendPlainText("My port Loacal is :" + QString("%1").arg(portlocal));
+
+    connect(ui->lineEdit, SIGNAL(returnPressed()), this, SLOT(on_send_clicked()));
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::leeSocket()
+{
+    while(mSocket->hasPendingDatagrams()){
+        QByteArray datagram;
+        datagram.resize(mSocket->pendingDatagramSize());
+
+        QHostAddress sender;
+        quint16 senderport;
+
+        mSocket->readDatagram(datagram.data(), datagram.size(),
+                              &sender, &senderport);
+        ui->plainTextEdit->appendPlainText(QString(datagram) +"( ip: " + sender.toString()
+                                           + "port: " +  QString("%1").arg(senderport));
+      }
+}
+
+
+void Widget::on_Connect_clicked()
+{
+    ui->lcdNumber->setDigitCount(11);
+
+    host = ui->lineEdit->text().section(',', 0, 0);
+    port = ui->lineEdit->text().section(',', 1, 1).toInt();
+
+    ui->lcdNumber_2->display(host.toString());
+    ui->lcdNumber->display(port);
+
+    ui->lineEdit->clear();
+    ui->plainTextEdit->clear();
+}
+
+
+void Widget::on_send_clicked()
+{
+    mSocket->writeDatagram(ui->lineEdit->text().toLatin1(), host, port);
+    ui->lineEdit->clear();
+}
+
